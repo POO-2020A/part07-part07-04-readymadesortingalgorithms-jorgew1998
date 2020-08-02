005d853051dc44eb5131d1a877cc069335cd9245
jorgew1998 <66254573+jorgew1998@users.noreply.github.com>
2020-08-02 15:02:53 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,52 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[]  numbers = {8, 3, 7, 9, 1, 2, 4};
+        Main.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        String[] names = {"jorge", "washington", "alba"};
+        Main.sort(names);
+        System.out.println(Arrays.toString(names));
+
+        ArrayList<Integer> numbersTwo = new ArrayList<>();
+        numbersTwo.add(9);
+        numbersTwo.add(5);
+        numbersTwo.add(3);
+        numbersTwo.add(15);
+        Main.sortIntegers(numbersTwo);
+        System.out.println(numbersTwo);
+
+
+        ArrayList<String> namesTwo = new ArrayList<>();
+        namesTwo.add("venegas");
+        namesTwo.add("kathy");
+        namesTwo.add("alba");
+        namesTwo.add("jimena");
+        Main.sortStrings(namesTwo);
+        System.out.println(namesTwo);
     }
+    
+    public static void sort(int[] array) {
+    Arrays.sort(array);
+  }
+
+  public static void sort(String[] array) {
+    Arrays.sort(array);
+    
+  }
+
+  public static void sortIntegers(ArrayList<Integer> integers) {
+    Collections.sort(integers);
+  }
+
+  public static void sortStrings(ArrayList<String> strings) {
+    Collections.sort(strings);
+  }
 
 }
